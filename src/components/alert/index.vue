d783e2ec364b1e0477bdf63d56a9798a633dfa86
zhaojintian <729234283@qq.com>
2019-03-13 23:42:05 +0800

    优化

---
+++ src/components/alert/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div
+      v-show="visible"
+      class="c-overlay"
+      @click="$emit('cancel')">
+    </div>
+    <div
+      v-show="visible"
+      class="c-alert">
+      <div
+        v-if="title"
+        class="c-alert__title u-tac fs40">{{ title }}
+      </div>
+      <div class="c-alert__body u-tac fs32">
+        <slot />
+      </div>
+      <ul class="c-alert__foot o-grid u-tac fs36">
+        <li
+          class="o-grid__cell"
+          @click="$emit('confirm')">
+          确定
+        </li>
+        <li
+          class="o-grid__cell c4"
+          @click="$emit('cancel')">
+          取消
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CAlert',
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style
+  lang="scss"
+  src="./styles/index.scss">
+</style>
